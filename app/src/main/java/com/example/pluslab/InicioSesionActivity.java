c26b316dca992e349404b1faaa112d1f2a83d76d
Gamaliel De los santos <gama.amc@gmail.com>
2020-10-15 23:23:50 -0500

    Pag inicio e inicio de sesión v1

---
+++ app/src/main/java/com/example/pluslab/InicioSesionActivity.java
@@ -0,0 +1,14 @@
+package com.example.pluslab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_sesion_activity);
+    }
+}
